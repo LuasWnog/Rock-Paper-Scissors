8b9d3bbfdf3efebee1de1178c299424f1fdcf97c
LuasWnog <luaswnog@gmail.com>
2024-12-18 00:08:03 +0800

    Reworked the RockPaperScissors to include buttons

--- javascript.js
+++ javascript.js
@@ -1,57 +1,95 @@
-let humanAnswer = "";
-let computerAnswer = "";
 let humanScore = 0;
 let computerScore = 0;
+let winConfirm = 0;
 
-function getComputerChoice() {
-    let computerAnswerNumber = Math.floor(Math.random() * 3);
-    if (computerAnswerNumber == 0) {
-        computerAnswer = "ROCK";
-    }
-    else if (computerAnswerNumber == 1) {
-        computerAnswer = "PAPER";
-    }
-    else {
-        computerAnswer = "SCISSORS";
-    }
-    return computerAnswerNumber;
+const btnRock = document.querySelector(".btnRock");
+const btnPaper = document.querySelector(".btnPaper");
+const btnScissors = document.querySelector(".btnScissors");
+const playAgain = document.querySelector(".playAgain");
+const scoreboard = document.querySelector(".scoreboard");
+const results = document.querySelector(".results");
+
+btnRock.addEventListener("click", playRoundRock);
+btnPaper.addEventListener("click", playRoundPaper);
+btnScissors.addEventListener("click", playRoundScissors);
+playAgain.addEventListener("click", resetScore);
+
+function scoreBoard() {
+    scoreboard.textContent = `Score: Human - ${humanScore} : Computer - ${computerScore}`;
 }
 
-function getHumanChoice () {
-    humanAnswer = prompt("Enter your play: ").toUpperCase();
-    if (humanAnswer == "ROCK") {
-        return 0;
+function winCheck() {
+    if (humanScore == 5) {
+        winConfirm = 1;
+        results.textContent = "GAME OVER! YOU WIN!";
     }
-    else if (humanAnswer == "PAPER") {
-        return 1;
-    }
-    else if (humanAnswer == "SCISSORS") {
-        return 2;
-    }
-    else {
-        alert("Your input is not valid");
-    }
-}
-
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        let difference = humanChoice-computerChoice;
-        if (difference == -2 || difference == 1) {
-            alert(`YOU WIN! ${humanAnswer} BEATS ${computerAnswer}!`);
-            humanScore = humanScore + 1;
-        }
-        else if (difference == -1 || difference == 2) {
-            alert(`YOU LOSE! ${humanAnswer} LOSES TO ${computerAnswer}`);
-            computerScore = computerScore + 1;
-        }
-        else {
-            alert(`YOU TIE! ${humanAnswer} TIES WITH ${computerAnswer}`);
-        }
-    }
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-        alert(`HUMAN: ${humanScore}, COMPUTER: ${computerScore}`);
+    else if (computerScore == 5) {
+        winConfirm = 1;
+        results.textContent = "GAME OVER! YOU LOSE!";
     }
 }
 
-playGame();
+function resetScore() {
+    humanScore = 0;
+    computerScore = 0;
+    winConfirm = 0;
+    results.textContent = "START PLAYING!";
+    scoreBoard();
+}
+
+function playRoundRock() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (winConfirm == 0) {
+        if (computerChoice == 0) {
+            results.textContent = ("YOU TIE! ROCK TIES WITH ROCK!");
+        }
+        else if (computerChoice == 1) {
+            results.textContent = ("YOU LOSE! ROCK LOSES TO PAPER");
+            computerScore += 1;
+        }
+        else if (computerChoice == 2) {
+            results.textContent = ("YOU WIN! ROCK BEATS SCISSORS");
+            humanScore += 1;
+        }
+        scoreBoard();
+        winCheck();
+    }
+}
+
+function playRoundPaper() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (winConfirm == 0) {
+        if (computerChoice == 0) {
+            results.textContent = ("YOU TIE! PAPER TIES WITH PAPER!");
+        }
+        else if (computerChoice == 1) {
+            results.textContent = ("YOU LOSE! PAPER LOSES TO SCISSORS!");
+            computerScore += 1;
+        }
+        else if (computerChoice == 2) {
+            results.textContent = ("YOU WIN! PAPER BEATS ROCK!");
+            humanScore += 1;
+        }
+        scoreBoard();
+        winCheck();
+    }
+}
+
+function playRoundScissors() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (winConfirm == 0) {
+        if (computerChoice == 0) {
+            results.textContent = ("YOU TIE! SCISSORS TIES WITH SCISSORS!");
+        }
+        else if (computerChoice == 1) {
+            results.textContent = ("YOU LOSE! SCISSORS LOSES TO ROCK!");
+            computerScore += 1;
+        }
+        else if (computerChoice == 2) {
+            results.textContent = ("YOU WIN! SCISSORS BEATS PAPER!");
+            humanScore += 1;
+        }
+        scoreBoard();
+        winCheck();
+    }
+}
